ae132d2c90a48be79422a57fb2d2826e4f03d089
Lucas Henrique <lucasmula125@gmail.com>
2025-05-02 00:43:41 -0300

    Better comptime code usage

--- src/main.v
+++ src/main.v
@@ -4,6 +4,15 @@ import os
 import readline { read_line }
 import net.http
 
+fn build_path(elems []string) string {
+	sep := $if windows {
+		'\\'
+	}$else{
+		'/'
+	}
+	return elems.join(sep)
+}
+
 fn main() {
 	default_path := os.home_dir()
 	mut install_path := read_line('Type the installation path (defaults to ${default_path}):\n') or {
@@ -14,7 +23,7 @@ fn main() {
 		install_path = default_path
 	}
 	
-	install_path = "${install_path}\\Noir Server"
+	install_path = build_path([install_path, 'Noir Server'])
 	println('Installing on ${install_path}')
 	if !os.exists(install_path) {
 		os.mkdir("${install_path}") or {
@@ -23,15 +32,17 @@ fn main() {
 		}
 	}
 	println("Downloading precompiled binary from Github...")
-	mut base_url := 'https://github.com/ZillaZ/noir_cicd_server/releases/download/release/cicd_server-unix64'
-	$if windows {
-		base_url = 'https://github.com/ZillaZ/noir_cicd_server/releases/download/release/cicd_server-win64.exe'
+	base_url := $if linux {
+		'https://github.com/ZillaZ/noir_cicd_server/releases/download/release/cicd_server-unix64'
+	} $else {
+		'https://github.com/ZillaZ/noir_cicd_server/releases/download/release/cicd_server-win64.exe'
 	}
-	mut binary_name := 'noir_server'
-	$if windows {
-		binary_name = 'noir_server.exe'
+	binary_name := $if linux {
+		'noir_server'
+	} $else {
+		'noir_server.exe'
 	}
-	http.download_file_with_progress(base_url, "${install_path}\\${binary_name}") or {
+	http.download_file_with_progress(base_url, build_path([install_path, binary_name])) or {
 		println('Failed to download executable.\n${err}')
 		return
 	}
@@ -39,10 +50,11 @@ fn main() {
 		println("Unable to create config dir.\n${err}")
 		return
 	}
-	if !os.exists("${config_path}\\Noir\\Server") {
-		os.mkdir("${config_path}\\Noir") or {}
-		os.mkdir("${config_path}\\Noir\\Server") or {}
+	server_config_path := build_path([config_path, 'Noir', 'Server'])
+	if !os.exists(server_config_path) {
+		os.mkdir(build_path([config_path, 'Noir'])) or {}
+		os.mkdir(server_config_path) or {}
 	}
-	println('Created config directories at ${config_path}\\Noir\\Server')
+	println('Created config directories at ${server_config_path}')
 	println("You're almost ready! Just add ${install_path} to your PATH.\nRead about server configuration on https://github.com/ZillaZ/noir_cicd_server")
 }
